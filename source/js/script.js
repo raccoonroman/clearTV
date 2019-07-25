021d9f6be16524ecb5fc21aa5630be626e08fd3a
Роман <kromanjs@gmail.com>
2019-07-25 20:37:22 +0300

    add new sections

--- source/js/script.js
+++ source/js/script.js
@@ -1,16 +1,69 @@
 "use strict";
 
+// countdown start of code
+var countDownDate = new Date("July 25, 2019 21:00:00").getTime();
+var countDownNode1 = document.getElementById("countdown-desktop");
+var countDownNode2 = document.getElementById("countdown-mobile");
+var colonNode = "<span class=\"countdown__colon\"> : </span>";
+var countDownNodeContent = countDownNode1.innerHTML;
 
-var hamburger = document.querySelector(".hamburger");
-// var navMain = document.querySelector('.header-nav__list');
+var getHours = function(distance) {
+  return Math.floor((distance / (1000 * 60 * 60)));
+}
 
+var getMinutes = function(distance) {
+  return Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+}
 
-hamburger.addEventListener('click', function() {
-  if (hamburger.classList.contains('open')) {
-    hamburger.classList.remove('open');
-    // navMain.classList.remove('header-nav__list--active');
-  } else {
-    hamburger.classList.add('open');
-    // navMain.classList.add('header-nav__list--active');
+var getSeconds = function(distance) {
+  return Math.floor((distance % (1000 * 60)) / 1000);
+}
+
+var build2Digits = function(fun, distance) {
+  return fun(distance) >= 10 ? fun(distance) : "0" + fun(distance);
+}
+
+var build2DigitsNode = function(fun, distance) {
+  return "<span class=\"countdown__digits\">" + build2Digits(fun, distance) + "</span>";
+}
+
+var getTimer = function(distance) {
+  return build2DigitsNode(getHours, distance) + colonNode + build2DigitsNode(getMinutes, distance) + colonNode + build2DigitsNode(getSeconds, distance);
+}
+
+// Update the count down every 1 second
+var updateTime = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  countDownNode1.innerHTML = getTimer(distance);
+  countDownNode2.innerHTML = getTimer(distance);
+
+  var unlock = function(distance) {
+    if (distance <= 1000) {
+      clearInterval(updateTime);
+      countDownNode1.innerHTML = countDownNodeContent;
+      countDownNode2.innerHTML = countDownNodeContent;
+    }
   }
+
+  unlock(distance);
+}, 1000);
+//countdown end of code
+
+
+
+//slider with thumbnail
+$(document).ready(function(){
+  $('.about__slider-for').slick({
+    slidesToShow: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.about__slider-nav'
+  });
+  $('.about__slider-nav').slick({
+    slidesToShow: 4,
+    arrows: false,
+    asNavFor: '.about__slider-for',
+    focusOnSelect: true
+  });
 });
